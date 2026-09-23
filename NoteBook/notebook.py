8593e97eccb0fb32373cf6f6fa701ea0b822b654
hao-OON <328691454+hao-OON@users.noreply.github.com>
2026-09-23 10:18:38 +0800

    ignore IDE config and local data files

--- NoteBook/notebook.py
+++ NoteBook/notebook.py
@@ -3,21 +3,21 @@ def mywrite():
     notes = input("请输入今天的记录：")
     mytime = datetime.datetime.now()
     s = mytime.strftime("%Y-%m-%d %H:%M:%S")
-    with open("notes.txt","a",encoding="utf-8") as f:
+    with open("notes.json","a",encoding="utf-8") as f:
         f.write(s+" | "+notes+"\n")
 
 def myread():
-    with open("notes.txt","r",encoding="utf-8") as f:
+    with open("notes.json","r",encoding="utf-8") as f:
         test = f.readlines()
         for i in test:
             print(i)
 while True:
-    n = int(input("(1 记，2 看，0 退出)\n请选择你需要的操作："))
-    if n == 1:
+    n = input("(1 记，2 看，0 退出)\n请选择你需要的操作：")
+    if n == '1':
         mywrite()
-    elif n == 2:
+    elif n == '2':
         myread()
-    elif n == 0:
+    elif n == '0':
         break
     else:
-        continue
+        print("您输入的内容不符合要求，请重新输入：")
