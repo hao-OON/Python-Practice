17d5f702f4c0fb733447e6912f91000489d693b7
hao-OON <328691454+hao-OON@users.noreply.github.com>
2026-09-23 12:10:41 +0800

    split notebook into text and json versions

--- NoteBook/notebook_json.py
+++ NoteBook/notebook_json.py
@@ -1,28 +1,3 @@
-'''
-import datetime
-def mywrite():
-    notes = input("请输入今天的记录：")
-    mytime = datetime.datetime.now()
-    s = mytime.strftime("%Y-%m-%d %H:%M:%S")
-    with open("notes.json","a",encoding="utf-8") as f:
-        f.write(s+" | "+notes+"\n")
-
-def myread():
-    with open("notes.json","r",encoding="utf-8") as f:
-        test = f.readlines()
-        for i in test:
-            print(i)
-while True:
-    n = input("(1 记，2 看，0 退出)\n请选择你需要的操作：")
-    if n == '1':
-        mywrite()
-    elif n == '2':
-        myread()
-    elif n == '0':
-        break
-    else:
-        print("您输入的内容不符合要求，请重新输入：")
-'''
 import datetime
 import json
 def myread():
@@ -56,6 +31,3 @@ while True:
         break
     else:
         print("您输入的内容不符合要求，请重新输入：")
-
-
-
