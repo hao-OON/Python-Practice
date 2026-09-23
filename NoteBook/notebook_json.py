dee0e6aba42d9f1484ca64c491a7cfd42d304496
hao-OON <328691454+hao-OON@users.noreply.github.com>
2026-09-23 14:56:21 +0800

    add time and keyword search

--- NoteBook/notebook_json.py
+++ NoteBook/notebook_json.py
@@ -1,11 +1,13 @@
 import datetime
 import json
+
 def myread():
     with open("notes.json","r",encoding="utf-8") as data:
         records = json.load(data)
         for i in records:
             print(i['time'])
             print(i['content'])
+            print("-----------------------------")
 
 def mywrite():
     mytime = datetime.datetime.now()
@@ -21,12 +23,34 @@ def mywrite():
     with open("notes.json","w",encoding="utf-8") as data1:
         json.dump(records,data1,ensure_ascii=False,indent=2)
 
+def mysearch():
+    with open("notes.json","r",encoding="utf-8") as f:
+        records = json.load(f)
+
+    timesearch = input("请输入你想查询的时间：")
+    keys = input("请输入你想查询的关键词：")
+    if timesearch == '' and keys == '':
+        print("你没有填写查询条件，请重新操作")
+        return
+    judge = False
+    for i in records:
+        if (timesearch != "" and timesearch in i['time']) or (keys != "" and keys in i['content']):
+            print(i['time'])
+            print(i['content'])
+            print("-----------------------------")
+            judge = True
+
+    if not judge:
+        print("没有搜索到你查找的内容")
+
 while True:
-    n = input("(1 记，2 看，0 退出)\n请选择你需要的操作：")
+    n = input("(1 记，2 看，3 搜，0 退出)\n请选择你需要的操作：")
     if n == '1':
         mywrite()
     elif n == '2':
         myread()
+    elif n == '3':
+        mysearch()
     elif n == '0':
         break
     else:
