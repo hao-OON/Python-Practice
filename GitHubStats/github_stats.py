952651ec7168fd340e070a9e7eb0b56deb13ffce
hao-OON <328691454+hao-OON@users.noreply.github.com>
2026-09-24 14:27:52 +0800

    add script to get people's repos from github

--- GitHubStats/github_stats.py
+++ GitHubStats/github_stats.py
@@ -1,23 +1,27 @@
 import datetime
 import json
 import requests
+def d_resp(repo,code):
 
-resp = requests.get('https://api.github.com/users/hao-OON/repos',timeout=10)
+        stime = repo['updated_at']
+        st1 = datetime.datetime.strptime(stime, '%Y-%m-%dT%H:%M:%SZ')
+        nowtime = st1 + datetime.timedelta(hours=8)
+        nowtime1 = nowtime.strftime('%Y-%m-%d %H:%M:%S')
 
+        new_resp = {}
+        new_resp['status_code'] = code
+        new_resp['name'] = repo['name']
+        new_resp['updated_at_bj'] = nowtime1
+        new_resp['stargazers_count'] = repo['stargazers_count']
+
+        return new_resp
+user = input('请输入你要请求的用户：')
+resp = requests.get(f'https://api.github.com/users/{user}/repos',timeout=10)
 resp1 = json.loads(resp.text)
 
-stime = resp1[0]['updated_at']
-st1 = datetime.datetime.strptime(stime, '%Y-%m-%dT%H:%M:%SZ')
-nowtime = st1 + datetime.timedelta(hours=8)
-nowtime1 = nowtime.strftime('%Y-%m-%d %H:%M:%S')
+li = []
+for i in resp1:
+        li.append(d_resp(i,resp.status_code))
 
-new_resp = {}
-new_resp['status_code'] = resp.status_code
-new_resp['name'] = resp1[0]['name']
-new_resp['updated_at_bj'] = nowtime1
-new_resp['stargazers_count'] = resp1[0]['stargazers_count']
-
-records = []
-records.append(new_resp)
 with open('repos.json', 'w',encoding="utf-8") as outfile:
-        json.dump(records, outfile, ensure_ascii=False, indent=2)
+        json.dump(li, outfile, ensure_ascii=False, indent=2)
